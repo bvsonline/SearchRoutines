cb6626ea6249ab6368e5558f9be0c7d90eb786a0
Venkata B <bvsonline@gmail.com>
2019-04-18 22:20:22 -0700

    implement binary search

--- SearchRoutines/main.cpp
+++ SearchRoutines/main.cpp
@@ -1,15 +1,51 @@
-//
 //  main.cpp
-//  SearchRoutines
+//  BitWiseOperations
 //
 //  Created by venkata bethamcharla on 4/18/19.
 //  Copyright © 2019 VENKATA. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int binarySearch(int x, int *ref_array, int size);
+
+int main()
+{
+    int ref_array_0[10] = {4, 5, 6, 7, 8, 9, 10, 15, 142, 1250};
+    int searchLoc = binarySearch(141, &ref_array_0[0], 10);
+    printf("searchLoc: %d \n", searchLoc);
+
+    int ref_array_1[10] = {400, 500, 600, 700, 800, 900, 1000, 1501, 14223, 125034};
+    searchLoc = binarySearch(500, &ref_array_1[0], 10);
+    printf("searchLoc: %d \n", searchLoc);
+    
     return 0;
+}
+
+
+int binarySearch(int x, int ref_array[], int size)
+{
+    int low, high, mid;
+    low = 0;
+    high = size-1;
+    
+    while (low <= high)
+    {
+        mid = (low+high)/2;
+        
+        if (x > ref_array[mid])
+        {
+            low = mid+1;
+        }
+        else if (x < ref_array[mid])
+        {
+            high = mid-1;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    
+    return -1;
 }
